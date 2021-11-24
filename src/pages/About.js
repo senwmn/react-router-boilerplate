9a2a37e3d6fb78fb33a54fc3120543b40831c680
Sid N <sid.e.newman@gmail.com>
2021-11-24 14:47:13 +0000

    Update About.js
    test

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React from "react";
 
 const About = () => {
-    return (
-        <div className="content">
-            <h1>About page</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At officiis voluptatem suscipit reprehenderit numquam qui excepturi! Reprehenderit consectetur cupiditate ab esse soluta, sapiente voluptatem impedit quae fugit id eos dolore aperiam officia minus deleniti praesentium pariatur, explicabo nisi optio ratione nemo. Quidem, atque est laboriosam sunt cum voluptate consectetur facere tempore, iure, facilis molestiae numquam beatae minima impedit eligendi. Ratione, dicta! Libero veniam culpa eum facilis sed. Dignissimos placeat reprehenderit, vero quas quis repudiandae fuga quod molestiae ab, veniam sequi a optio expedita commodi nam. Dicta ab mollitia facilis aperiam dignissimos deleniti, cumque impedit, consequatur, hic quo quibusdam magni tempore.</p>
-        </div>
-    )
-}
+  return (
+    <div className="content">
+      <h1>About page test</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. At officiis
+        voluptatem suscipit reprehenderit numquam qui excepturi! Reprehenderit
+        consectetur cupiditate ab esse soluta, sapiente voluptatem impedit quae
+        fugit id eos dolore aperiam officia minus deleniti praesentium pariatur,
+        explicabo nisi optio ratione nemo. Quidem, atque est laboriosam sunt cum
+        voluptate consectetur facere tempore, iure, facilis molestiae numquam
+        beatae minima impedit eligendi. Ratione, dicta! Libero veniam culpa eum
+        facilis sed. Dignissimos placeat reprehenderit, vero quas quis
+        repudiandae fuga quod molestiae ab, veniam sequi a optio expedita
+        commodi nam. Dicta ab mollitia facilis aperiam dignissimos deleniti,
+        cumque impedit, consequatur, hic quo quibusdam magni tempore.
+      </p>
+    </div>
+  );
+};
 
 export default About;
